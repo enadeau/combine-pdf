197f08603f649b5205c755ddf1f812d6e1178fac
antoinelab01 <66086247+antoinelab01@users.noreply.github.com>
2020-06-03 13:12:27 -0400

    Update split.py
    The new version support formats with multiple page ranges, like 23-25,34,5-6

--- split.py
+++ split.py
@@ -8,36 +8,48 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 class WorkPacket(NamedTuple):
     title: str
-    start: int
-    end: int
+    ranges: list
 
-
-def extract_range(inputpdf: PdfFileReader, start: int, end: int) -> PdfFileWriter:
+def extract_ranges(inputpdf: PdfFileReader, ranges: list) -> PdfFileWriter:
     """
     Returns the pdf with the given page range. Page number must be 0-based and
     both are included.
     """
     output = PdfFileWriter()
-    for i in range(start, end + 1):
-        output.addPage(inputpdf.getPage(i))
+    for start,end in ranges:
+        for i in range(start, end + 1):   
+            output.addPage(inputpdf.getPage(i))
     return output
 
+def str_to_range(str_range: str):
+    if "-" in str_range:
+        start, end = map(int, str_range.split("-"))
+    else:
+        start = int(str_range)
+        end = int(str_range)
+    return (start-1,end-1)
 
 def extract_work_packet(range_file: str) -> Iterator[WorkPacket]:
-    with open(range_file, "r") as f:
+    with open(file, "r") as f:
         for line in f:
-            title, str_range = map(str.strip, line.split(":"))
-            start, end = map(int, str_range.split("-"))
-            yield (WorkPacket(title, start - 1, end - 1))
-
+            title, str_ranges = map(str.strip, line.split(":"))
+            ranges_list = map(str_to_range, (str_ranges.split(",")))
+            yield (WorkPacket(title, ranges_list))
 
 parser = argparse.ArgumentParser(
     "PDF splitter", description="Splits a pdf in sub pdf based on ranges"
 )
 parser.add_argument(
-    "ranges_file", help="File with range. Format is `title:start-end`"
+    "ranges_file", help="File with range. Format is `title:start0-end0,...,startk-endk`"
 )
 parser.add_argument("pdf_file", help="The pdf file")
+
+def extract_work_packet(range_file: str) -> Iterator[WorkPacket]:
+    with open(range_file, "r") as f:
+        for line in f:
+            title, str_ranges = map(str.strip, line.split(":"))
+            ranges_list = map(str_to_range, (str_ranges.split(",")))
+            yield (WorkPacket(title, ranges_list))
 
 
 def main():
@@ -47,7 +59,7 @@ def main():
         inputpdf = PdfFileReader(input_stream)
         for wp in extract_work_packet(args.ranges_file):
             logger.info(f"Processing {wp.title}")
-            output = extract_range(inputpdf, wp.start, wp.end)
+            output = extract_ranges(inputpdf, wp.ranges)
             with open(wp.title + ".pdf", "wb") as outputStream:
                 output.write(outputStream)
             logger.info("Done.")
